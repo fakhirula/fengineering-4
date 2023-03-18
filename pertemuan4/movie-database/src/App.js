a7422cc13c80b5352804b7ad6933326508bfdeff
fakhirula <fakhirula27@gmail.com>
2023-03-18 16:49:28 +0700

    reupload pertemuan4-fix banget

--- pertemuan4/movie-database/src/App.js
+++ pertemuan4/movie-database/src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Hello from './components/Hello';
+import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
-      <Hello name="Fakhirul" major="Informatika" />
-      <Hello name="Akmal" major="Informasi" />
+      <Main />
       <Footer />
     </div>
   );
---
+++ pertemuan4/movie-database/src/components/Main.js
@@ -0,0 +1,10 @@
+import Hello from './Hello';
+
+const Main = () => (
+  <div>
+    <Hello name="Fakhirul" major="Informatika" />
+    <Hello name="Akmal" major="Informasi" />
+  </div>
+);
+
+export default Main;

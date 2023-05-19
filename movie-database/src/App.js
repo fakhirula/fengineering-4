513fe11e92b1277eabb20de3c1a9a4d361f6e0cd
fakhirula <fakhirula27@gmail.com>
2023-05-19 10:04:01 +0700

    pertemuan9

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -5,18 +5,20 @@ import Popular from './pages/movie/Popular';
 import NowPlaying from './pages/movie/NowPlaying';
 import TopRated from './pages/movie/TopRated';
 import Layout from './Layout';
+// import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/create" element={<CreateMovie />}></Route>
-        <Route path="/movie/popular" element={<Popular />}></Route>
-        <Route path="/movie/now" element={<NowPlaying />}></Route>
-        <Route path="/movie/top" element={<TopRated />}></Route>
-      </Routes>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/movie/create" element={<CreateMovie />}></Route>
+            <Route path="/movie/popular" element={<Popular />}></Route>
+            <Route path="/movie/now" element={<NowPlaying />}></Route>
+            <Route path="/movie/top" element={<TopRated />}></Route>
+        </Routes>
     </Layout>
+    // <Counter/>
   );
 }
 
--- movie-database/src/components/Hero/Hero.js
+++ movie-database/src/components/Hero/Hero.js
@@ -1,26 +1,42 @@
+import { useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 function Hero() {
+    const [movie, setMovie] = useState("");
+
+    
+
+    useEffect(() => {
+        async function fetchMovie() {
+          const url = 'https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590';
+
+          const response = await fetch(url);
+          const data = await response.json();
+
+          setMovie(data);
+        }
+
+        fetchMovie()
+    }, []);
+
     return (
       <div className={styles.hero__bg}>
         <div className={styles.hero__bgimage}></div>
         <div className={styles.container}>
           <div className={styles.hero}>
             <div className={styles.hero__left}>
-              <h2 className={styles.hero__title}>Spiderman</h2>
-              <p className={styles.hero__genre}>Action, Drama</p>
+              <h2 className={styles.hero__title}>{movie.Title}</h2>
+              <p className={styles.hero__genre}>{movie.Genre}</p>
               <p className={styles.hero__description}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Assumenda distinctio necessitatibus placeat tenetur aut et
-                facilis ducimus rem quis aliquid!
+                {movie.Plot}
               </p>
               <button className={styles.hero__button}>Watch</button>
             </div>
             <div className={styles.hero__right}>
               <img
                 className={styles.hero__image}
-                src="https://picsum.photos/536/354"
-                alt=""
+                src={movie.Poster}
+                alt={movie.Title}
               />
             </div>
           </div>

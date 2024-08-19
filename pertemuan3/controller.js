758a918f11b5db99b0735b79d5e7bd080bd90e4d
fakhirula <fakhirula27@gmail.com>
2024-08-19 10:50:42 +0700

    pertemuan3

---
+++ pertemuan3/controller.js
@@ -0,0 +1,11 @@
+import users from "./data.js";
+
+function index() {
+    console.log("### looping data");
+}
+
+function store() {
+    console.log("### tambah data");
+}
+
+export { index, store }; 
